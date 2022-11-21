0585b40c8052b61943a1bfc3f2df72b7cfd6f9fe
katie <kj0203@gmail.com>
2022-11-21 18:42:48 -0500

    hey hey

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
           <h1> Hi, I'm Katie and I'm a Cyber Security Analyst. </h1>
           <h3 style={{fontWeight:"300"}}>
             {" "}
-            Other interests include baking, my dog, and becoming the best hacker in the world.
+            Interests include baking, biking, and my dog Teddy.
 
           </h3>
 
