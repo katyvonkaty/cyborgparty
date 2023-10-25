4aad764fa443ba9a790706b47c2c4460b4fe4e6e
y2katie <kj0203@gmail.com>
2023-10-25 15:18:59 -0400

    updated

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,7 +2,6 @@ import React from "react";
 import Log4J from "./Log4J";
 import Wifi from "./Wifi";
 import { Image, Card } from "semantic-ui-react";
-
 import Infiltrate from "./Infiltrate";
 import Network from "./Network";
 import Volatility from "./Volatility";
@@ -12,7 +11,6 @@ import SQL from "./SQL";
 import Click from "./Click";
 import NewList from "./NewList";
 import Burp from "./Burpsuite";
-
 import honey from "../images/honey.png";
 import physical from "../images/physical.jpg";
 import vol from "../images/vol.jpg";
@@ -31,94 +29,172 @@ import { Button, Icon, Item, Label, Grid } from "semantic-ui-react";
 const Options = (props) => {
   return (
     <Card.Group itemsPerRow={2}>
-            <Card style={{padding:"20px"}}>
-              <Image
-                src="https://securityqueens.co.uk/wp-content/uploads/2020/06/burpsuite.png"
-                wrapped
-                ui={false}
-              />
-              <Card.Content>
-                <Card.Header>Burpsuite</Card.Header>
-                <Card.Meta>
-                  <span className="date">Joined in 2015</span>
-                </Card.Meta>
-                <Card.Description>
-                  <span className="cinema">
-                    Burp Suite is a software security application used for
-                    penetration testing of web applications. Both a free and a
-                    paid version of the software are available. The software is
-                    developed by the company PortSwigger
-                  </span>{" "}
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-              <Item.Extra>
-                  <Label>Web Security</Label>
-                  <Label>Vunerable</Label>
-                  <Label>HTML</Label>
-                </Item.Extra>
-                <Button
-                  component={vol}
-                  href="/Burp"
-                  to="/Burp"
-                  color="grey"
-                  floated="left"
-                >
-                  View Project
-                  <Icon name="right chevron" />
-                </Button>
-              </Card.Content>
-            </Card>
+      <Card style={{ padding: "20px" }}>
+        <Image
+          src="https://securityqueens.co.uk/wp-content/uploads/2020/06/burpsuite.png"
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>Burpsuite</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            <span className="cinema">
+              Burp Suite is a software security application used for penetration
+              testing of web applications. Both a free and a paid version of the
+              software are available. The software is developed by the company
+              PortSwigger
+            </span>{" "}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Item.Extra>
+            <Label>Web Security</Label>
+            <Label>Vunerable</Label>
+            <Label>HTML</Label>
+          </Item.Extra>
+          <Button
+            component={vol}
+            href="/Burp"
+            to="/Burp"
+            color="grey"
+            floated="left"
+          >
+            View Project
+            <Icon name="right chevron" />
+          </Button>
+        </Card.Content>
+      </Card>
 
+      <Card style={{ padding: "20px" }}>
+        <Image
+          src="https://cdn.invicti.com/statics/img/ogimage/clickjacking-attacks.png"
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>Clickjacking</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            <span className="cinema">
+              SQL injection (SQLi) is a web security vulnerability that allows
+              an attacker to interfere with the queries that an application
+              makes to its database.
+            </span>{" "}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Item.Content>
+            <Item.Extra>
+              <Label>Web Security</Label>
+              <Label>Vunerable</Label>
+              <Label>HTML</Label>
+            </Item.Extra>
+            <Button
+              component={vol}
+              href="/Click"
+              to="/Click"
+              color="grey"
+              floated="left"
+            >
+              View Project
+              <Icon name="right chevron" />
+            </Button>
+          </Item.Content>
+        </Card.Content>
+      </Card>
+    <Card.Group itemsPerRow={2} >
+    <Card style={{ padding: "20px" }}>
+        <Image
+          src="https://academy.avast.com/hs-fs/hubfs/New_Avast_Academy/SQL%20injection/What%20is%20a%20SQL%20injection.png?width=660&name=What%20is%20a%20SQL%20injection.png"
 
-      
-
-
-        
-          <Card style={{padding:"20px"}}>
-              <Image
-                src="https://cdn.invicti.com/statics/img/ogimage/clickjacking-attacks.png" wrapped
-                ui={false}
-              />
-              <Card.Content>
-                <Card.Header>Clickjacking</Card.Header>
-                <Card.Meta>
-                  <span className="date">Joined in 2015</span>
-                </Card.Meta>
-                <Card.Description>
-                  <span className="cinema">
-                  SQL injection (SQLi) is a web security vulnerability that
-                  allows an attacker to interfere with the queries that an
-                  application makes to its database.
-                  </span>{" "}
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-              <Item.Content>
-                <Item.Extra>
-                  <Label>Web Security</Label>
-                  <Label>Vunerable</Label>
-                  <Label>HTML</Label>
-                </Item.Extra>
-                <Button
-                  component={vol}
-                  href="/Click"
-                  to="/Click"
-                  color="grey"
-                  floated="left"
-                >
-                  View Project
-                  <Icon name="right chevron" />
-                </Button>
-              </Item.Content>
-              </Card.Content>
-            </Card>
-            </Card.Group>
-      
-      
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>SQL Injection</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            <span className="cinema">
+              SQL injection (SQLi) is a web security vulnerability that allows
+              an attacker to interfere with the queries that an application
+              makes to its database.
+            </span>{" "}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+        <Item.Content>
+      <Item.Extra>
+        <Label>Web Security</Label>
+        <Label>Vunerable</Label>
+        <Label>HTML</Label>
+      </Item.Extra>
+      <Button
+        component={vol}
+        href="/SQL"
+        to="/SQL"
+        color="grey"
+        floated="left"
+      >
+        View Project
+        <Icon name="right chevron" />
+      </Button>
+    </Item.Content>
+        </Card.Content>
+      </Card>
+      <Card style={{ padding: "20px" }}>
+        <Image
+          src="https://assets.website-files.com/5ff66329429d880392f6cba2/61f251b52a583fa4149b98c0_CSRF%20Preview.jpg"
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>Cross Site Request Forgery</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            <span className="cinema">
+            CSRF can be executed a number of ways but most browsers are
+        equipped to stop the attack way before it starts. Here I'm
+        exploring potenial ways a bad actor could use CRSF via token
+        validation, samesite cookie restrictions and referer-based
+        CSRF defenses.
+            </span>{" "}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+        <Item.Content>
+      <Item.Extra>
+        <Label>Web Security</Label>
+        <Label>Vunerable</Label>
+        <Label>HTML</Label>
+      </Item.Extra>
+      <Button
+        component={vol}
+        href="/CRSF"
+        to="/CRSF"
+        color="grey"
+        floated="left"
+      >
+        View Project
+        <Icon name="right chevron" />
+      </Button>
+    </Item.Content>
+        </Card.Content>
+      </Card>
+    </Card.Group>
+    </Card.Group>
 
 
   );
 };
 
 export default Options;
+
